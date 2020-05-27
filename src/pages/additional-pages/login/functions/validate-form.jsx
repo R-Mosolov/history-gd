88bb7502914cc5ceb3270ff96a50880a668cb4ae
Roman Mosolov <R.V.Mosolov@ya.ru>
2020-05-27 21:29:12 +0300

    #60@1h34m: try to add login through React states

--- src/pages/additional-pages/login/functions/validate-form.jsx
+++ src/pages/additional-pages/login/functions/validate-form.jsx
@@ -1,11 +1,15 @@
 import FormValidator from "../../../../lib/form-validator/form-validator";
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const formValidator = new FormValidator();
 
 // Validating registration data
 function validateForm() {
   formValidator.checkLoginFields("login-page__email", "login-page__password");
+
+  return <Link to="/" className="notDisabled">Link</Link>;
 }
 
 export default validateForm;
--- src/pages/additional-pages/login/login.jsx
+++ src/pages/additional-pages/login/login.jsx
@@ -1,51 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import "./login.css";
 import "../../../lib/form-validator/render-error";
 import validateForm from "./functions/validate-form";
+import users from "../../../data/users";
 
-function Login() {
-  return (
-    <div className="login mt-5 mb-5 d-flex justify-content-center align-items-center container">
-      <div className="w-50">
-        <h1 className="pt-5">Вход в систему</h1>
+class Login extends Component {
+  constructor() {
+    super();
 
-        <form id="login-form">
-          <div className="d-flex flex-column mb-2">
-            <label>Email (электронная почта)</label>
-            <input
-              id="login-page__email"
-              className="form-control"
-              type="email"
-              min="3"
-              max="75"
-              placeholder="MV.Lomonosov@msu.ru"
-            />
+    this.state = {
+      userList: users,
+      email: "admin@test.ru",
+      password: "Initial State Password",
+      isCorrectData: false
+    };
+  }
+
+  checkAuthData() {
+    this.setState({
+      email: document.getElementById("login-page__email").value,
+      password: document.getElementById("login-page__password").value
+    });
+
+    users.forEach(user => {
+        if (user.email === this.state.email) {
+          return <Link to="/manuscripts" />
+        }
+    });
+
+    // users.forEach(user => console.log(`user.email: ${user.email}`));
+    // console.log(`this.state.email: ${this.state.email}`);
+    // console.log(`this.state.password: ${this.state.password}`);
+  }
+
+  render() {
+    return (
+        <div className="login mt-5 mb-5 d-flex justify-content-center align-items-center container">
+          <div className="w-50">
+            <h1 className="pt-5">Вход в систему</h1>
+
+            <form id="login-form">
+              <div className="d-flex flex-column mb-2">
+                <label>Email (электронная почта)</label>
+                <input
+                    id="login-page__email"
+                    className="form-control"
+                    type="email"
+                    min="3"
+                    max="75"
+                    placeholder="MV.Lomonosov@msu.ru"
+                    value="Email"
+                />
+              </div>
+
+              <div className="d-flex flex-column">
+                <label>Пароль</label>
+                <input
+                    id="login-page__password"
+                    className="form-control"
+                    type="password"
+                    min="8"
+                    max="50"
+                    placeholder="********"
+                    value="Password"
+                />
+              </div>
+            </form>
+
+            <button
+                id="login-button"
+                className="mt-3 btn btn-success btn-block"
+                onClick={() => this.checkAuthData()}
+            >
+              Войти
+            </button>
           </div>
-
-          <div className="d-flex flex-column">
-            <label>Пароль</label>
-            <input
-              id="login-page__password"
-              className="form-control"
-              type="password"
-              min="8"
-              max="50"
-              placeholder="********"
-            />
-          </div>
-        </form>
-
-        <button
-          id="login-button"
-          className="mt-3 btn btn-success btn-block"
-          onClick={() => validateForm()}
-        >
-          Войти
-        </button>
-      </div>
-    </div>
-  );
+        </div>
+    );
+  }
 }
 
 export default Login;
