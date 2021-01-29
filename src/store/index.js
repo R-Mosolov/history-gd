bb23cf2e74fb51da1e4bffa014919fda6096f5c1
Roman Mosolov <R.V.Mosolov@ya.ru>
2021-01-29 04:54:33 +0300

    Improve tcsconfig.json file

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-exports.__esModule = true;
-const redux_1 = require('redux');
-const reducer_1 = require('./reducer');
-
-const store = redux_1.createStore(reducer_1.default);
-exports.default = store;
--- src/store/initial-state.js
+++
@@ -1,5 +0,0 @@
-exports.__esModule = true;
-const manuscripts_base_1 = require('../data/manuscripts/manuscripts-base');
-
-const initialState = manuscripts_base_1.default;
-exports.default = initialState;
