7af624daa574a482c357563324fedb01db97d98f
Roman Mosolov <R.V.Mosolov@ya.ru>
2021-02-22 20:15:30 +0300

    Add feature to make text bold, italic, and underline, refactoring styles

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -3,16 +3,24 @@ import React, { Component } from 'react';
 
 // Icons
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import FormatBoldIcon from '@material-ui/icons/FormatBold';
+import FormatItalicIcon from '@material-ui/icons/FormatItalic';
+import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined';
 
 // Styles
 import './editor.scss';
 
 export default class Editor extends Component {
+  makeTextBold = () => document.execCommand('bold');
+  makeTextItalic = () => document.execCommand('italic');
+  makeTextUnderline = () => document.execCommand('underline');
+
   returnInJson() {
     const result = document
-      .getElementsByClassName('editor__tools_active-input')[0]
+      .getElementsByClassName('editor__content_active-input')[0]
       .innerHTML;
     
+    console.log('returnInJson');
     console.log(result);
   }
 
@@ -21,18 +29,34 @@ export default class Editor extends Component {
       <section className="editor">
         <div className="editor__container">
           <section className="editor__tools">
-            <div className="editor__tools_container">
-              <AddBoxIcon />
+            <FormatBoldIcon
+              fontSize="large"
+              className="editor__icon"
+              onClick={this.makeTextBold}
+            />
+            <FormatItalicIcon
+              fontSize="large"
+              className="editor__icon"
+              onClick={this.makeTextItalic}
+            />
+            <FormatUnderlinedIcon
+              fontSize="large"
+              className="editor__icon"
+              onClick={this.makeTextUnderline}
+            />
+          </section>
+          
+          <section className="editor__content">
+            <div className="editor__content_container">
+              <AddBoxIcon fontSize="large" />
             </div>
             <div
-              className="editor__tools_active-input"
+              className="editor__content_active-input"
               contentEditable
             >
               <b>Bold</b> and <i>italic</i>
             </div>
           </section>
-          
-          <section className="editor__content"></section>
           
           <section className="editor__actions">
             <button
