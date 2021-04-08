696636a0624e56723c6d04a0dd65f3f63ede7aa2
Roman Mosolov <R.V.Mosolov@ya.ru>
2021-04-08 12:59:20 +0300

    Run Upstream

--- src/data/languages/README.md
+++
@@ -1,4 +0,0 @@
-Transliteration from Russian in Latin is based on Library of Congress Standard:
-
-1. https://www.loc.gov/catdir/cpso/romanization/russian.pdf
-2. https://translit.cc
--- src/server/index.ts
+++
@@ -1,6 +0,0 @@
-import dbConfig from './db-config';
-import firestore from './firestore';
-import auth from './auth';
-import storage from './storage';
-
-export { dbConfig, firestore, auth, storage };
